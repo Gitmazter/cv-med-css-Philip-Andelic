e7877ac443131e3c19cd8af8ea196e94af283600
Philip Andelic <100753538+Gitmazter@users.noreply.github.com>
2022-11-09 21:45:38 +0700

    Patch 1.1
    Changelog:
    Added Clock to projects
    Added Calculator to projects
    
    Pending fixes:
    Need to separate Metamask Script too its own file for About.html

--- docs/myscript.js
+++ docs/myscript.js
@@ -9,6 +9,40 @@ function closeNav() {
 
 }
 
+function showTime () {
+  let date = new Date();
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+  let session = "AM";
+
+   if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    let time = `${h}:${m}:${s} ${session}`;
+
+    document.getElementById("clock_display").innerText = time;
+    document.getElementById("clock_display").textContent = time;
+    
+    setTimeout(showTime, 1000);
+}
+
+showTime();
+
+function calculator () {
+  
+}
+
 
 
 
